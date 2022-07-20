7bf94608c4677efe9327b49f19f9fecbb73b8ed4
BekkerV8 <valkenshtein@gmail.com>
2022-07-20 07:20:30 -0700

    Changed testes shouldReturnAddGames

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -20,7 +20,7 @@ public class GameStoreTest {
         GameStore store = new GameStore();
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
         Game game2 = new Game("Нетология Основы Git", "Аркады", store);
-        assertTrue(store.containsGame(game) && store.containsGame(game2));
+        assertTrue(store.containsGame(game));
     }
 
     @Test
